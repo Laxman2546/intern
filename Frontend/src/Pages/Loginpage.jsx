abe1c24aafaf029688cd099b433ae12589f94fbd
ELLA LAKSHMAN <laxmannani960@gmail.com>
2025-08-01 21:43:57 +0530

    fixed errors

--- Frontend/src/Pages/Loginpage.jsx
+++ Frontend/src/Pages/Loginpage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import HomeImg from "../assets/login.svg";
 import FbImg from "../assets/fb.png";
 import google from "../assets/google.jpg";
